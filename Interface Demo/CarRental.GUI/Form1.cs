f6c7f60527e5ac2c30e6c86646fb3739e7176676
herkommer <m@herkommer.co.uk>
2019-10-18 16:58:27 +0200

    added EF support

--- Interface Demo/CarRental.GUI/Form1.cs
+++ Interface Demo/CarRental.GUI/Form1.cs
@@ -12,9 +12,21 @@ namespace CarRental.GUI
 {
     public partial class Form1 : Form
     {
+        BerrasEntities anka = new BerrasEntities();
+
         public Form1()
         {
             InitializeComponent();
+
+            //Testar att skapa en ny bil
+            anka.Cars.Add(new Car() { Make = "BMW", Model = "530d" });
+            anka.SaveChanges();
+
+            //Ta kontakt med EF och se till att hämta data!
+            foreach (Car item in anka.Cars)
+            {
+                listBox1.Items.Add(item);
+            }
         }
     }
 }
---
+++ Interface Demo/CarRental.GUI/MyClasses.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.GUI
+{
+    public partial class Car 
+    {
+        public override string ToString()
+        {
+            return string.Format("{0} {1}", Make, Model);
+        }
+    }
+}
