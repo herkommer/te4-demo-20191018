3b0932d4b27c36215e425615129bf8488bd6d3e5
herkommer <m@herkommer.co.uk>
2019-10-18 15:59:50 +0200

    added animals interface

---
+++ Interface Demo/Animals/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IAnimalHospitalBasic anka;
+            anka = new Dog() { PetName = "Karo" };
+            Console.WriteLine(anka.PetName);
+
+            anka = new Cat() { PetName = "Flisan" };
+            Console.WriteLine(anka.PetName);
+        }
+    }
+
+    public interface IAnimalHospitalBasic
+    {
+        string ElectronicId();
+        string PetName { get; set; }
+    }
+
+
+    public class Horse
+    {
+
+    }
+    public class Dog : IAnimalHospitalBasic
+    {
+        public string PetName { get; set; }
+
+        public string ElectronicId()
+        {
+            return "IUYN87612KJH";
+        }
+    }
+    public class Cat : IAnimalHospitalBasic
+    {
+        private string _name;
+        public string PetName { get { return _name; } set { _name = value; } }
+
+        public string ElectronicId()
+        {
+            return "HAJSH15";
+        }
+    }
+    public class Pig
+    {
+
+    }
+}
